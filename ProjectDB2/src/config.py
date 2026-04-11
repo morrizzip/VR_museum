38aae368e0ca168c2980425318ddb598c6e29b1b
morrizzip <49508606+morrizzip@users.noreply.github.com>
2026-04-11 11:51:29 +0300

    Delete ProjectDB2 directory

--- ProjectDB2/src/config.py
+++
@@ -1,9 +0,0 @@
-import os
-
-class Config:
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'dev-secret-key-change-in-production'
-    DATABASE_URI = 'sqlite:///src.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
-    FLASK_DEBUG = os.getenv('FLASK_DEBUG')
-    UPLOAD_FOLDER = os.getenv('UPLOAD_FOLDER')
-    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URI')
